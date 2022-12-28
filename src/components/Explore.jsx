30e0823839e7722a6d7968ff543a6737b8e0d51d
Bhavya Patel <bhavyapatel0811@gmail.com>
2022-12-28 13:48:03 -0500

    trial commit

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -5,7 +5,7 @@ const Explore = () => {
     <div>
       <div className="row">
         <div className="explore-container">
-          <h1 className="explore-heading">Explore more Books</h1>
+          <h1 className="explore-heading">Bhavya Explore more Books</h1>
           <button className="explore-button">
             <Link to="/books" className="explore-button-link">Explore</Link>
           </button>
